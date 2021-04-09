6a4fdde60b7bae8b1ab7b1f3652e71d1a9c3793f
Aaron Espinoza <aaronespinozz@gmail.com>
2021-04-09 18:29:36 +0200

    first step

---
+++ bier-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>MY bier app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
